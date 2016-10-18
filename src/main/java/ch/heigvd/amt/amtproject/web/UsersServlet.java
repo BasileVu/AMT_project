c1290ca5355f28c004464e635b91e1659e86b550
Basile Vu <basile.vu@gmail.com>
2016-10-19 00:30:31 +0200

    Cleaning

--- src/main/java/ch/heigvd/amt/amtproject/web/UsersServlet.java
+++ src/main/java/ch/heigvd/amt/amtproject/web/UsersServlet.java
@@ -25,7 +25,6 @@ public class UsersServlet extends HttpServlet {
     public static final String USED_JSP = "users.jsp";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO
         request.getRequestDispatcher(JSP_FOLDER + USED_JSP).forward(request, response);
     }
 }
