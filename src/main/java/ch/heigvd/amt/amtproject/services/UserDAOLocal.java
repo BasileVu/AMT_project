d5f2764a1d106cc8e032ad64efeb8e15e1d98285
Basile Vu <basile.vu@gmail.com>
2016-10-18 17:45:10 +0200

    Adding id in User

--- src/main/java/ch/heigvd/amt/amtproject/services/UserDAOLocal.java
+++ src/main/java/ch/heigvd/amt/amtproject/services/UserDAOLocal.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Local
 public interface UserDAOLocal {
-    void create(User u) throws RuntimeException;
+    void create(String username, String password) throws RuntimeException;
     User get(String username) throws RuntimeException;
     List<User> getAll() throws RuntimeException;
     void update(User u) throws RuntimeException;
